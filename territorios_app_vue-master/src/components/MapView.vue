a228bc5506cd5d8268a25f5efe7405f94d9a9f74
Diegogo87 <49370289+Diegogo87@users.noreply.github.com>
2020-11-16 17:02:37 +0100

    Add files via upload

---
+++ territorios_app_vue-master/src/components/MapView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script
+import { "mapbox-config.js" } from "@vue/territorio_app_vue";
+</script>
+<style>
+  body {
+        margin: 0;
+        padding: 0;
+    }
+    
+    #map {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        margin-top: 40px;
+    }
+    
+    .mapboxgl-popup-content {
+        position: relative;
+        border-radius: 10px!important;
+        box-shadow: -4px 3px 3px rgba(0, 0, 0, 0.7)!important;
+        padding: 15px 14px 6px !important;
+        padding-left: 9px!important;
+        pointer-events: auto;
+    }
+    
+    #borrar,
+    #enviar {
+        width: 50px;
+        height: 20px;
+        margin-bottom: 8px;
+        border-radius: 7px;
+    }
+</style>
