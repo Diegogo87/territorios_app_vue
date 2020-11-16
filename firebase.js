c8da76156e56c0dfa8ae5fb9ff20cb8529797316
Diegogo87 <49370289+Diegogo87@users.noreply.github.com>
2020-11-16 16:34:06 +0100

    Firebase config

---
+++ firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDt26QSzLS2mTzUfYXtWMzQWhm8s8qcx3o",
+    authDomain: "territorios-a1708.firebaseapp.com",
+    databaseURL: "https://territorios-a1708.firebaseio.com",
+    projectId: "territorios-a1708",
+    storageBucket: "territorios-a1708.appspot.com",
+    messagingSenderId: "473257612033",
+    appId: "1:473257612033:web:2db7faef9941ec7fe50100",
+    measurementId: "G-6RNL9BXXRL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
